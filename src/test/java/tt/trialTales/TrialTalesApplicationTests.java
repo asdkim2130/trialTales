020b9875e03fc8b7e4233992a1a7e364210003aa
HYOJIN <hjhwang08@naver.com>
2025-01-21 15:45:16 +0900

    initial commit

---
+++ src/test/java/tt/trialTales/TrialTalesApplicationTests.java
@@ -0,0 +1,13 @@
+package tt.trialTales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialTalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
