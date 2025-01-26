86aa4a9d69ea487cc718aeefd8bc6f7af15b0da9
HYOJIN <soothing06@gmail.com>
2025-01-26 17:35:01 +0900

    fix : SecurityConfig 수정

--- src/main/java/tt/SecurityConfig.java
+++ src/main/java/tt/SecurityConfig.java
@@ -2,17 +2,22 @@ package tt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().permitAll(); // 모든 요청 허용
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll());
+
         return http.build();
     }
 }
