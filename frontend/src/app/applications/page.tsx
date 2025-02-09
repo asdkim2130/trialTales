486512bd39ebe71f44d0c26fae409eda1bad4651
HYOJIN <soothing06@gmail.com>
2025-02-10 00:52:52 +0900

    feat : campaign 임시데이터 배열로 변경, page.tsx에서 id값으로 데이터 불러오기 구현

--- frontend/src/app/applications/page.tsx
+++ frontend/src/app/applications/page.tsx
@@ -4,11 +4,19 @@ import { campaignSample } from "./tempData";
 
 
 export default function ApplicationPage() {
+    const campaignId = 1;
+
+    const tempCampaignSample = campaignSample.find(campaign => campaign.id === campaignId);
+
+    if (!tempCampaignSample) {
+        return <div>캠페인을 찾을 수 없습니다.</div>;
+    }
+
     return (
         <div className="bg-white rounded-lg shadow-lg overflow-hidden max-w-[1000px] mx-auto flex min-h-screen items-center">
             <div className="w-1/2 p-6 flex flex-col">
-                <ImageBox campaignSample={campaignSample} />
-                <CampaignInfo campaignSample={campaignSample} />
+                <ImageBox campaignSample={tempCampaignSample} />
+                <CampaignInfo campaignSample={tempCampaignSample} />
             </div>
             <div className="w-1/2 p-6 border-l">
                 <ApplicationRequest />
